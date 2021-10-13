5e39a5307a1c9d07f8d10f48349e628d833724a5
YeonhyukKim <brankein13@gm.gist.ac.kr>
2021-10-13 18:14:43 +0900

    cross certificate 완료

--- src/main/java/MainActivity.java
+++ src/main/java/MainActivity.java
@@ -46,6 +46,10 @@ public class MainActivity {
     public KeyPair rootCA_B_KeyPair;
     public X509Certificate rootCA_B_Cert;
 
+    public X500Name subCA1_B_SubjectName;
+    public KeyPair subCA1_B_KeyPair;
+    public X509Certificate subCA1_B_Cert;
+
     public X500Name subCA2_B_SubjectName;
     public KeyPair subCA2_B_KeyPair;
     public X509Certificate subCA2_B_Cert;
@@ -55,7 +59,7 @@ public class MainActivity {
     public X509Certificate leaf_B_Cert;
 
     public X500Name cross_SubjectName;
-    public KeyPair cross_KeyPair;
+    // public KeyPair cross_KeyPair;
     public X509Certificate cross_Cert;
 
     public static void main(String[] args) {
@@ -68,8 +72,46 @@ public class MainActivity {
             makeRootCA_A();
             makeCertChain_A();
             makeRootCA_B();
+            makeCertChain_B();
+            crossCert();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public void crossCert() throws Exception {
+        BigInteger cross_Serial = new BigInteger(Long.toString(Math.abs(new SecureRandom().nextLong())));
+
+        X500Name cross_Issuer = rootCA_A_SubjectName;
+        cross_SubjectName = new X500Name("CN=cross_cert");
+
+        PKCS10CertificationRequestBuilder cross_p10Builder = new JcaPKCS10CertificationRequestBuilder(cross_SubjectName, subCA1_B_KeyPair.getPublic());
+        JcaContentSignerBuilder cross_csrBuilder = new JcaContentSignerBuilder("SHA256withRSA").setProvider("BC");
+
+        ContentSigner cross_csrContentSigner = cross_csrBuilder.build(rootCA_A_KeyPair.getPrivate());
+        PKCS10CertificationRequest cross_csr = cross_p10Builder.build(cross_csrContentSigner);
+
+        X509v3CertificateBuilder cross_Builder = new X509v3CertificateBuilder(
+                cross_Issuer, cross_Serial, startDate, endDate, cross_csr.getSubject(), cross_csr.getSubjectPublicKeyInfo());
+        X509CertificateHolder cross_CertHolder = cross_Builder.build(cross_csrContentSigner);
+        cross_Cert = new JcaX509CertificateConverter().setProvider("BC").getCertificate(cross_CertHolder);
+
+        // print Certificate
+        printCertificate("cross Cert", cross_Cert);
+
+        // chain validation
+        List<Certificate> certChain = new ArrayList<Certificate>();
+        certChain.add(leaf_B_Cert);
+        certChain.add(subCA2_B_Cert);
+        certChain.add(cross_Cert);
+        certChain.add(rootCA_A_Cert);
+        if (verifyChain(certChain)) {
+            System.out.println("A Chain Verified!");
+            System.out.println();
+        } else {
+            System.out.println("A Chain Not Verified...");
+            System.out.println();
+            throw new Exception();
         }
     }
 
@@ -88,30 +130,30 @@ public class MainActivity {
 
         BigInteger rootSerialNum = new BigInteger(Long.toString(Math.abs(new SecureRandom().nextLong())));
 
-        rootCA_A_SubjectName = new X500Name("CN=RootCA");
+        rootCA_A_SubjectName = new X500Name("CN=RootCA_A");
         ContentSigner rootCertContentSigner = new JcaContentSignerBuilder("SHA256withRSA").setProvider("BC").build(rootCA_A_KeyPair.getPrivate());
         X509v3CertificateBuilder rootCertBuilder = new JcaX509v3CertificateBuilder(rootCA_A_SubjectName, rootSerialNum, startDate, endDate, rootCA_A_SubjectName, rootCA_A_KeyPair.getPublic());
 
         X509CertificateHolder rootCertHolder = rootCertBuilder.build(rootCertContentSigner);
         rootCA_A_Cert = new JcaX509CertificateConverter().setProvider("BC").getCertificate(rootCertHolder);
 
-        printCertificate("TestRootCert", rootCA_A_Cert);
+        printCertificate("RootCA_A", rootCA_A_Cert);
 
     }
 
     public void makeRootCA_B() throws Exception {
-        rootCA_A_KeyPair = generateKeyPairWithRSA();
+        rootCA_B_KeyPair = generateKeyPairWithRSA();
 
         BigInteger rootSerialNum = new BigInteger(Long.toString(Math.abs(new SecureRandom().nextLong())));
 
-        rootCA_A_SubjectName = new X500Name("CN=RootCA");
-        ContentSigner rootCertContentSigner = new JcaContentSignerBuilder("SHA256withRSA").setProvider("BC").build(rootCA_A_KeyPair.getPrivate());
-        X509v3CertificateBuilder rootCertBuilder = new JcaX509v3CertificateBuilder(rootCA_A_SubjectName, rootSerialNum, startDate, endDate, rootCA_A_SubjectName, rootCA_A_KeyPair.getPublic());
+        rootCA_B_SubjectName = new X500Name("CN=RootCA_B");
+        ContentSigner rootCertContentSigner = new JcaContentSignerBuilder("SHA256withRSA").setProvider("BC").build(rootCA_B_KeyPair.getPrivate());
+        X509v3CertificateBuilder rootCertBuilder = new JcaX509v3CertificateBuilder(rootCA_B_SubjectName, rootSerialNum, startDate, endDate, rootCA_B_SubjectName, rootCA_B_KeyPair.getPublic());
 
         X509CertificateHolder rootCertHolder = rootCertBuilder.build(rootCertContentSigner);
-        rootCA_A_Cert = new JcaX509CertificateConverter().setProvider("BC").getCertificate(rootCertHolder);
+        rootCA_B_Cert = new JcaX509CertificateConverter().setProvider("BC").getCertificate(rootCertHolder);
 
-        printCertificate("TestRootCert", rootCA_A_Cert);
+        printCertificate("RootCA_B", rootCA_B_Cert);
 
     }
 
@@ -164,7 +206,7 @@ public class MainActivity {
         leaf_A_KeyPair = generateKeyPairWithRSA();
         BigInteger leaf_A_Serial = new BigInteger(Long.toString(Math.abs(new SecureRandom().nextLong())));
 
-        X500Name leaf_A_Issuer = rootCA_A_SubjectName;
+        X500Name leaf_A_Issuer = subCA2_A_SubjectName;
         leaf_A_SubjectName = new X500Name("CN=Leaf_A");
 
         PKCS10CertificationRequestBuilder leaf_A_p10Builder = new JcaPKCS10CertificationRequestBuilder(leaf_A_SubjectName, subCA2_A_KeyPair.getPublic());
@@ -183,9 +225,13 @@ public class MainActivity {
          */
 
         // print certificate
+        System.out.println("########################################################################################################");
+        System.out.println("########################################################################################################");
         printCertificate("SubCA1_A", subCA1_A_Cert);
         printCertificate("SubCA2_A", subCA2_A_Cert);
         printCertificate("Leaf_A", leaf_A_Cert);
+        System.out.println("########################################################################################################");
+        System.out.println("########################################################################################################");
 
         /*
         ###############################################################################################################################################################
@@ -202,6 +248,102 @@ public class MainActivity {
             System.out.println();
         } else {
             System.out.println("A Chain Not Verified...");
+            System.out.println();
+            throw new Exception();
+        }
+    }
+
+    public void makeCertChain_B() throws Exception {
+        // Sub CA1 B
+        subCA1_B_KeyPair = generateKeyPairWithRSA();
+        BigInteger subCA1_B_Serial = new BigInteger(Long.toString(Math.abs(new SecureRandom().nextLong())));
+
+        X500Name subCA1_B_Issuer = rootCA_A_SubjectName;
+        subCA1_B_SubjectName = new X500Name("CN=SubCA1_B");
+
+        PKCS10CertificationRequestBuilder subCA1_B_p10Builder = new JcaPKCS10CertificationRequestBuilder(subCA1_B_SubjectName, subCA1_B_KeyPair.getPublic());
+        JcaContentSignerBuilder subCA1_B_csrBuilder = new JcaContentSignerBuilder("SHA256withRSA").setProvider("BC");
+
+        ContentSigner subCA1_B_csrContentSigner = subCA1_B_csrBuilder.build(rootCA_B_KeyPair.getPrivate());
+        PKCS10CertificationRequest subCA1_B_csr = subCA1_B_p10Builder.build(subCA1_B_csrContentSigner);
+
+        X509v3CertificateBuilder subCA1_B_Builder = new X509v3CertificateBuilder(
+                subCA1_B_Issuer, subCA1_B_Serial, startDate, endDate, subCA1_B_csr.getSubject(), subCA1_B_csr.getSubjectPublicKeyInfo());
+        X509CertificateHolder subCA1_B_CertHolder = subCA1_B_Builder.build(subCA1_B_csrContentSigner);
+        subCA1_B_Cert = new JcaX509CertificateConverter().setProvider("BC").getCertificate(subCA1_B_CertHolder);
+
+        /*
+        ###############################################################################################################################################################
+         */
+
+        // Sub CA2 B
+        subCA2_B_KeyPair = generateKeyPairWithRSA();
+        BigInteger subCA2_B_Serial = new BigInteger(Long.toString(Math.abs(new SecureRandom().nextLong())));
+
+        X500Name subCA2_B_Issuer = subCA1_B_SubjectName;
+        subCA2_B_SubjectName = new X500Name("CN=SubCA2_B");
+
+        PKCS10CertificationRequestBuilder subCA2_B_p10Builder = new JcaPKCS10CertificationRequestBuilder(subCA2_B_SubjectName, subCA2_B_KeyPair.getPublic());
+        JcaContentSignerBuilder subCA2_B_csrBuilder = new JcaContentSignerBuilder("SHA256withRSA").setProvider("BC");
+
+        ContentSigner subCA2_B_csrContentSigner = subCA2_B_csrBuilder.build(subCA1_B_KeyPair.getPrivate());
+        PKCS10CertificationRequest subCA2_B_csr = subCA2_B_p10Builder.build(subCA2_B_csrContentSigner);
+
+        X509v3CertificateBuilder subCA2_B_Builder = new X509v3CertificateBuilder(
+                subCA2_B_Issuer, subCA2_B_Serial, startDate, endDate, subCA2_B_csr.getSubject(), subCA2_B_csr.getSubjectPublicKeyInfo());
+        X509CertificateHolder subCA2_B_CertHolder = subCA2_B_Builder.build(subCA2_B_csrContentSigner);
+        subCA2_B_Cert = new JcaX509CertificateConverter().setProvider("BC").getCertificate(subCA2_B_CertHolder);
+
+        /*
+        ###############################################################################################################################################################
+         */
+
+        // Leaf A
+        leaf_B_KeyPair = generateKeyPairWithRSA();
+        BigInteger leaf_B_Serial = new BigInteger(Long.toString(Math.abs(new SecureRandom().nextLong())));
+
+        X500Name leaf_B_Issuer = subCA2_B_SubjectName;
+        leaf_B_SubjectName = new X500Name("CN=Leaf_B");
+
+        PKCS10CertificationRequestBuilder leaf_B_p10Builder = new JcaPKCS10CertificationRequestBuilder(leaf_B_SubjectName, subCA2_B_KeyPair.getPublic());
+        JcaContentSignerBuilder leaf_B_csrBuilder = new JcaContentSignerBuilder("SHA256withRSA").setProvider("BC");
+
+        ContentSigner leaf_B_csrContentSigner = leaf_B_csrBuilder.build(subCA2_B_KeyPair.getPrivate());
+        PKCS10CertificationRequest leaf_B_csr = leaf_B_p10Builder.build(leaf_B_csrContentSigner);
+
+        X509v3CertificateBuilder leaf_B_Builder = new X509v3CertificateBuilder(
+                leaf_B_Issuer, leaf_B_Serial, startDate, endDate, leaf_B_csr.getSubject(), leaf_B_csr.getSubjectPublicKeyInfo());
+        X509CertificateHolder leaf_B_CertHolder = leaf_B_Builder.build(leaf_B_csrContentSigner);
+        leaf_B_Cert = new JcaX509CertificateConverter().setProvider("BC").getCertificate(leaf_B_CertHolder);
+
+        /*
+        ###############################################################################################################################################################
+         */
+
+        // print certificate
+        System.out.println("########################################################################################################");
+        System.out.println("########################################################################################################");
+        printCertificate("SubCA1_B", subCA1_B_Cert);
+        printCertificate("SubCA2_B", subCA2_B_Cert);
+        printCertificate("Leaf_B", leaf_B_Cert);
+        System.out.println("########################################################################################################");
+        System.out.println("########################################################################################################");
+
+        /*
+        ###############################################################################################################################################################
+         */
+
+        // chain validation
+        List<Certificate> certChain = new ArrayList<Certificate>();
+        certChain.add(leaf_B_Cert);
+        certChain.add(subCA2_B_Cert);
+        certChain.add(subCA1_B_Cert);
+        certChain.add(rootCA_B_Cert);
+        if (verifyChain(certChain)) {
+            System.out.println("B Chain Verified!");
+            System.out.println();
+        } else {
+            System.out.println("B Chain Not Verified...");
             System.out.println();
             throw new Exception();
         }
@@ -250,18 +392,16 @@ public class MainActivity {
         String cert = new String(Base64.encode(certificate.getEncoded()));
         char[] certArr = cert.toCharArray();
 
-        System.out.println("####################################################");
         System.out.println(name + " Certificate Made!");
         System.out.println("-----BEGIN CERTIFICATE-----");
-        for(int i = 0; i < certArr.length; i++) {
-            System.out.print(certArr[i]);
-            if ((i + 1) % 65 == 0) {
-                System.out.println();
-            }
-        }
-        System.out.println();
+        System.out.println(cert);
+//        for(int i = 0; i < certArr.length; i++) {
+//            System.out.print(certArr[i]);
+//            if ((i + 1) % 65 == 0) {
+//                System.out.println();
+//            }
+//        }
         System.out.println("------END CERTIFICATE------");
-        System.out.println("####################################################");
         System.out.println();
     }
 }
